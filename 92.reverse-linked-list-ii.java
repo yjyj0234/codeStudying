a6761591d7df1278429528140d78944b30b042ab
yjyj0234 <yjyj0345@naver.com>
2026-04-27 17:27:23 +0900

    20260427

--- 92.reverse-linked-list-ii.java
+++ 92.reverse-linked-list-ii.java
@@ -18,6 +18,26 @@
 class Solution {
     public ListNode reverseBetween(ListNode head, int left, int right) {
         
+        ListNode dummy = new ListNode(0); //원래노드에 0 추가( 0->1->2->3 같은)
+        dummy.next = head;
+
+        ListNode prev = dummy;
+
+        for(int i=1; i<left; i++) {
+            prev = prev.next;
+        }
+
+        ListNode curr = prev.next;
+
+        for(int i=0; i<right-left; i++){
+            ListNode temp = curr.next;
+
+            curr.next = temp.next;
+            temp.next = prev.next;
+            prev.next = temp;
+        }
+
+        return dummy.next;
     }
 }
 // @lc code=end
