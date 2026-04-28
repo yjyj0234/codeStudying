a2e65d451a1aebd0e06c87e76a04e786e2eaf8a5
yjyj0234 <yjyj0345@naver.com>
2026-04-28 14:51:05 +0900

    20260428

---
+++ 26.remove-duplicates-from-sorted-array.java
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode id=26 lang=java
+ *
+ * [26] Remove Duplicates from Sorted Array
+ */
+
+// @lc code=start
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        //배열 앞쪽으로 중복되는 숫자 없이 정렬 ex) [1,1,2,2,3] -> [1,2,3,1,2]
+
+        int slow = 0;
+        int fast = 1;
+
+        while (fast <= nums.length-1 && slow <= nums.length-1) {
+            if(nums[slow] == nums[fast]) {
+                
+                fast++;
+            }else {
+                nums[slow+1] = nums[fast];
+                slow++;
+            }
+        }
+        return slow+1;
+    }
+}
+// @lc code=end
+
